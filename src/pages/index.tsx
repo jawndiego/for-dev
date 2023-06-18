f525ec86de16ccc056d980ca6ab37894ccafb8bf
iain nash <me@iain.in>
2023-06-18 19:42:53 -0400

    switch to zora collection

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import { Header } from "../components";
 import { MintFeltProjects } from "../components/mintFeltProjects";
 import { MintNFT } from "../components/mintNFT";
+import { MintZORANFT } from "../components/mintZoraProject";
 
 function Page() {
   return (
     <>
       <Header />
-      <MintFeltProjects />
+      <MintZORANFT address="0xf1a08861ba3c8788cda11481d807a9e5d3e3e114" />
     </>
   );
 }
